24efc1b81b6f4b7571f01ea50aa449fbbee3b7cc
Reddawn <35641620+forumevi@users.noreply.github.com>
2025-10-19 01:16:38 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
