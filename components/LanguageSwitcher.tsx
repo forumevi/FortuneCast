0377f963ec2c6a8efb9a0eeb14e5f8eaa0392bfa
Reddawn <35641620+forumevi@users.noreply.github.com>
2025-10-19 21:08:09 +0300

    Update LanguageSwitcher.tsx

--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -4,7 +4,6 @@ import { useTranslation } from "react-i18next";
 
 export default function LanguageSwitcher() {
   const { i18n } = useTranslation();
-
   return (
     <div>
       <button onClick={() => i18n.changeLanguage("en")}>EN</button>
